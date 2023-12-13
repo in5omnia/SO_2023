30a4e293a86734fec61db6eb701b7502b0bf296a
in5omnia <93283175+in5omnia@users.noreply.github.com>
2023-12-13 18:16:44 +0000

    initial commit

---
+++ constants.h
@@ -0,0 +1,9 @@
+#define MAX_RESERVATION_SIZE 256
+#define STATE_ACCESS_DELAY_MS 10
+
+// Number of args incluiding the arg0 (the program name)
+#define NUM_MANDATORY_ARGS 2
+#define DELAY_ARG_INDEX 2
+#define DIR_ARG_INDEX 1
+
+#define DT_REG 8
---
+++ main.c
@@ -0,0 +1,151 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/dirent.h>
+
+#include "constants.h"
+#include "operations.h"
+#include "parser.h"
+
+// ./ems <dir> <max jobs> <max threads> [delay]
+int main(int argc, char *argv[]) {
+  unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
+
+  if (argc < NUM_MANDATORY_ARGS) {
+	fprintf(stderr, "Invalid number of arguments\n");
+	return 1;
+  }
+
+  if (argc > NUM_MANDATORY_ARGS) {
+    char *endptr;
+    unsigned long int delay = strtoul(argv[DELAY_ARG_INDEX], &endptr, 10);
+
+    if (*endptr != '\0' || delay > UINT_MAX) {
+      fprintf(stderr, "Invalid delay value or value too large\n");
+      return 1;
+    }
+
+    state_access_delay_ms = (unsigned int)delay;
+  }
+
+  if (ems_init(state_access_delay_ms)) {
+    fprintf(stderr, "Failed to initialize EMS\n");
+    return 1;
+  }
+
+  char * dirpath = argv[DIR_ARG_INDEX];
+  DIR * dir = opendir(dirpath);
+  if (dir == NULL) {
+    fprintf(stderr, "Directory %s does not exists\n", dirpath);
+    return 1;
+  }
+
+  struct dirent* entry;
+
+  entry = readdir(dir);
+  while (entry) {
+	// read ALL files in the directory
+	if (entry->d_type == DT_REG) {
+		
+	}
+
+  	entry = readdir(dir);
+  }
+
+
+  while (1) {
+    unsigned int event_id, delay;
+    size_t num_rows, num_columns, num_coords;
+    size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
+
+    printf("> ");
+    fflush(stdout);
+
+    switch (get_next(STDIN_FILENO)) {
+      case CMD_CREATE:
+        if (parse_create(STDIN_FILENO, &event_id, &num_rows, &num_columns) != 0) {
+          fprintf(stderr, "Invalid command. See HELP for usage\n");
+          continue;
+        }
+
+        if (ems_create(event_id, num_rows, num_columns)) {
+          fprintf(stderr, "Failed to create event\n");
+        }
+
+        break;
+
+      case CMD_RESERVE:
+        num_coords = parse_reserve(STDIN_FILENO, MAX_RESERVATION_SIZE, &event_id, xs, ys);
+
+        if (num_coords == 0) {
+          fprintf(stderr, "Invalid command. See HELP for usage\n");
+          continue;
+        }
+
+        if (ems_reserve(event_id, num_coords, xs, ys)) {
+          fprintf(stderr, "Failed to reserve seats\n");
+        }
+
+        break;
+
+      case CMD_SHOW:
+        if (parse_show(STDIN_FILENO, &event_id) != 0) {
+          fprintf(stderr, "Invalid command. See HELP for usage\n");
+          continue;
+        }
+
+        if (ems_show(event_id)) {
+          fprintf(stderr, "Failed to show event\n");
+        }
+
+        break;
+
+      case CMD_LIST_EVENTS:
+        if (ems_list_events()) {
+          fprintf(stderr, "Failed to list events\n");
+        }
+
+        break;
+
+      case CMD_WAIT:
+        if (parse_wait(STDIN_FILENO, &delay, NULL) == -1) {  // thread_id is not implemented
+          fprintf(stderr, "Invalid command. See HELP for usage\n");
+          continue;
+        }
+
+        if (delay > 0) {
+          printf("Waiting...\n");
+          ems_wait(delay);
+        }
+
+        break;
+
+      case CMD_INVALID:
+        fprintf(stderr, "Invalid command. See HELP for usage\n");
+        break;
+
+      case CMD_HELP:
+        printf(
+            "Available commands:\n"
+            "  CREATE <event_id> <num_rows> <num_columns>\n"
+            "  RESERVE <event_id> [(<x1>,<y1>) (<x2>,<y2>) ...]\n"
+            "  SHOW <event_id>\n"
+            "  LIST\n"
+            "  WAIT <delay_ms> [thread_id]\n"  // thread_id is not implemented
+            "  BARRIER\n"                      // Not implemented
+            "  HELP\n");
+
+        break;
+
+      case CMD_BARRIER:  // Not implemented
+      case CMD_EMPTY:
+        break;
+
+      case EOC:
+        ems_terminate();
+        return 0;
+    }
+  }
+}
